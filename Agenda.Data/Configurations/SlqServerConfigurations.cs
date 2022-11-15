a1c99e075d9c015654086dd0049d6b2f309753c0
Desenvovedor Anderson Alves <andersondasilvaalvesdeveloper@gmail.com>
2022-11-15 20:16:38 -0300

    first commit

---
+++ Agenda.Data/Configurations/SlqServerConfigurations.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agenda.Data.Configurations
+{
+    /// <summary>
+    /// Classe para configuração da connectionstring
+    /// </summary>
+    public class SqlServerConfiguration
+    {
+        public static string GetConnectionString
+          // => @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=DBAgenda;Integrated Security=True;Connect Timeout=30;Encrypt=False; TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+          => @"Data Source=SQL8004.site4now.net;Initial Catalog=db_a8fbc8_dbagenda;User Id=db_a8fbc8_dbagenda_admin;Password=mAninhO01";
+
+    }
+
+}
+
+
---
+++ Agenda.Reports/Interfaces/IContatosReport.cs
@@ -0,0 +1,16 @@
+﻿using Agenda.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agenda.Reports.Interfaces
+{
+    public interface IContatosReport
+    {
+        //método abstrato!
+        byte[] CreateReport(List<Contato> contatos, Usuario usuario);
+
+    }
+}
